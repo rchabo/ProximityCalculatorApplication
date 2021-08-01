9af2ea9de4dd95143ec98f3748fc80ecb04332b3
rchabo <robin.chabouk.2016@live.rhul.ac.uk>
2021-08-01 11:49:32 +0100

    Initial commit

---
+++ src/main/java/com/ProximityCalculatorApplication/ProximityCalculatorApplication/ProximityCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.ProximityCalculatorApplication.ProximityCalculatorApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximityCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProximityCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProximityCalculatorApplication/ProximityCalculatorApplication/ProximityCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProximityCalculatorApplication.ProximityCalculatorApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximityCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
